73c778abd28f9b579a24ffd59593559b56493c14
Pablo Álvarez <palvarezs169@gmail.com>
2023-11-07 19:16:30 -0300

    add basic front-back interaction to check things are ok

---
+++ frontend/games-trading/src/components/Ping.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios';
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Ping',
+  data() {
+    return {
+      msg: 'Hello!',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5001/ping';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
