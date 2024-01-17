fdd238bb95e8076add23cad40bc12878ac7c1035
Pablo Álvarez <palvarezs169@gmail.com>
2024-01-17 19:03:00 -0300

    some initial frontend done and backend configs

--- backend/models.py
+++ backend/models.py
@@ -23,3 +23,71 @@ class UsersModel(db.Model):
 
     def __repr__(self):
         return f'<User {self.id}>'
+
+class GamesModel(db.Model):
+    __tablename__ = 'games'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    genre = db.Column(db.String(128), nullable=False)
+    year = db.Column(db.String(128), nullable=False)
+    platform = db.Column(db.String(128), nullable=False)
+
+    def __init__(self, name, genre, year, platform):
+        self.name = name
+        self.genre = genre
+        self.year = year
+        self.platform = platform
+
+    def __repr__(self):
+        return f'<Game {self.id}>'
+
+
+class OffersModel(db.Model):
+    __tablename__ = 'offers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+
+    def __init__(self, user_id, game_id):
+        self.user_id = user_id
+        self.game_id = game_id
+
+    def __repr__(self):
+        return f'<Offer {self.id}>'
+
+
+class WishesModel(db.Model):
+    __tablename__ = 'wishes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+
+    def __init__(self, user_id, game_id):
+        self.user_id = user_id
+        self.game_id = game_id
+
+    def __repr__(self):
+        return f'<Wish {self.id}>'
+
+class Exchanges(db.Model):
+    __tablename__ = 'exchanges'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_1_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_2_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    game_1_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+    game_2_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+    status = db.Column(db.String(128), nullable=False)
+
+    def __init__(self, user_1_id, user_2_id, game_1_id, game_2_id, status):
+        self.user_1_id = user_1_id
+        self.user_2_id = user_2_id
+        self.game_1_id = game_1_id
+        self.game_2_id = game_2_id
+        self.status = status
+
+    def __repr__(self):
+        return f'<Exchange {self.id}>'
