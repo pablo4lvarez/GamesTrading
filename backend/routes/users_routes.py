7bf083dd2291bcb6aa7f39f90cbc7f6b1d7bfa1f
Pablo Álvarez <palvarezs169@gmail.com>
2024-02-03 11:56:19 -0300

    add minimal db scheme and users CRUD done

--- backend/routes/users_routes.py
+++ backend/routes/users_routes.py
@@ -6,12 +6,16 @@ user_routes = Blueprint('user_routes', __name__)
 
 @user_routes.route('/users', methods=['GET'])
 def get_users():
-  #logic to get all users
-  pass
+  # Logic to get all users
+  users = UsersModel.query.all()
+  # Convert the users to a list of dictionaries
+  users_list = [{'name': user.name, 'lastname': user.lastname, 'email': user.email, 'phone': user.phone, 'location': user.location} for user in users]
+  return jsonify({"users": users_list})
+
 
 @user_routes.route('/users', methods=['POST'])
 def create_user():
-  #logic to create a user
+  # Logic to create a user
   if request.is_json:
     data = request.get_json()
     new_user = UsersModel(
@@ -29,8 +33,38 @@ def create_user():
   else:
     return jsonify({"error": "The request payload is not in JSON format"})
 
-@user_routes.route('/users/<id>', methods=['GET'])
-def get_user(id):
-  #logic to get a single user
-  pass
+@user_routes.route('/users/<id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_user(id):
+  user = UsersModel.query.get(id)
+  if request.method == 'GET':
+    # Logic to get a single user
+    if user:
+      return jsonify({"name": user.name, "lastname": user.lastname, "email": user.email, "phone": user.phone, "location": user.location})
+    else:
+      return jsonify({"error": "The user does not exist"})
+  
+  elif request.method == 'PUT':
+    # Logic to update a user
+    data = request.get_json()
+    if 'name' in data:
+        user.name = data['name']
+    if 'lastname' in data:
+        user.lastname = data['lastname']
+    if 'email' in data:
+        user.email = data['email']
+    if 'phone' in data:
+        user.phone = data['phone']
+    if 'password' in data:
+        user.password = data['password']
+    if 'location' in data:
+        user.location = data['location']
+    db.session.commit()
+    return jsonify({"message": f"User {user.name} successfully updated"})
+
+  elif request.method == 'DELETE':
+    # Logic to delete a user
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": f"User {user.name} successfully deleted."})
+
 
